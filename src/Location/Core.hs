0d5474bcb3bbd688e582180915cad2c0e769f3d9
Kyle Isom <kyle@imap.cc>
2018-11-06 07:16:54 -0800

    Update Core with currentLocation.
    It currently uses unsafePerformIO, which is a hack that I'd like to
    get rid of. It does, however, provide a useful starting point.

--- src/Location/Core.hs
+++ src/Location/Core.hs
@@ -3,10 +3,19 @@ module Location.Core where
 import Data.IORef as IORefs
 import Geodetics.Altitude as Alt
 import Geodetics.Geodetic as Geo
-import Numeric.Units.Dimensional.Prelude (meter, (*~))
+-- import Numeric.Units.Dimensional.Prelude (meter, (*~))
+import Numeric.Units.Dimensional.Prelude
+import qualified Prelude as P
+import System.IO.Unsafe (unsafePerformIO)
 
+-- | Degrees, minutes and seconds into radians.  Copied from the
+-- | tests for the geodetics package.
+dms :: Int -> Int -> Double -> Dimensionless Double
+dms d m s = fromIntegral d *~ degree + fromIntegral m *~ arcminute + s *~ arcsecond
+
+-- TODO: figure out how to do this while still retaining safety.
 currentPosition :: IORefs.IORef (Geo.Geodetic Geo.WGS84)
-currentPosition = IORefs.newIORef
+currentPosition = unsafePerformIO (IORefs.newIORef (Geo.Geodetic (dms 37 48 15.7068) (dms 22 16 15.9996) (9 *~ metre) Geo.WGS84))
 
 parseCoords :: String -> Maybe (Geo.Geodetic Geo.WGS84)
 parseCoords = Geo.readGroundPosition Geo.WGS84 
@@ -17,3 +26,9 @@ parseCoords = Geo.readGroundPosition Geo.WGS84
 -- maybeSetAltitude Nothing pos = pos
 -- maybeSetAltitude _ Nothing = Nothing
 -- maybeSetAltitude (Just alt) (Just pos) = Nothing -- Alt.setAltitude (alt *~ meter) (Just pos)
+
+data Coordinates = Coordinates {
+      latitude :: Double
+    , longitude :: Double
+    , altitude :: Double
+}
